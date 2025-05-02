86a8846272e5ea7966924184407e34e5b281a3c5
Alexander Miranda <alexanderdeveloperweb@gmail.com>
2025-05-02 17:38:17 +0000

    Commit para a Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1> Kamilla, se você me ama, dá uma risadinha! Rs  </h1>
+    return <h1> Kamilla, se você me ama, dá uma risadinha! S2  </h1>
 }
 
 export default Home;

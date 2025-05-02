9a5bd8c13f6a8390f55267fd8c935b7ff6166c64
Alexander Miranda <alexanderdeveloperweb@gmail.com>
2025-05-02 18:13:08 +0000

    Exibir Mudanças

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1> Kamilla, que horas você marcou com a moça? </h1>
+    return <h1> Eu Amo Você <span style="color: red;">&hearts;</span> </h1>
 }
 
 export default Home;
